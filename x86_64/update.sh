e52cf32adefd04e487b49c5923464b97cd514840
Stas Fiduchi <glavos@protonmail.com>
2021-02-27 12:57:44 +0200

    Update.

--- x86_64/update.sh
+++ x86_64/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# rsync -cauv --delete --info=stats2 --exclude={"caur.files","*.files.tar*","caur.db","*.db.tar*"} /var/cache/pacman/caur/ /home/cretm/app/dev.ctlos.ru/ctlos-aur
+# rsync -cauv --delete --info=stats2 --exclude={"caur.files","*.files.tar*","caur.db","*.db.tar*"} /var/cache/pacman/caur/ /home/cretm/app/dev.glavos.ru/glavos-aur
 
 # find './' -maxdepth 1 -type f -regex '.*\.\(zst\|xz\)' -exec gpg -b '{}' \;
 # find './' -type f -exec gpg --pinentry-mode loopback --passphrase=${GPG_PASS} -b '{}' \;
@@ -9,30 +9,30 @@
 # ./update.sh -add
 # systemctl --user start kbfs
 # ./update.sh -all
-# surge --project ../ --domain https://ctlos.surge.sh
+# surge --project ../ --domain https://glavos.surge.sh
 
-# https://osdn.net/projects/ctlos/storage/ctlos_repo/x86_64/
-# https://github.com/ctlos/ctlos_repo/tree/master/x86_64
-# https://cvc.keybase.pub/ctlos_repo/x86_64
-# https://ctlos.surge.sh
+# https://osdn.net/projects/glavos/storage/glavos_repo/x86_64/
+# https://github.com/glavos/glavos_repo/tree/master/x86_64
+# https://cvc.keybase.pub/glavos_repo/x86_64
+# https://glavos.surge.sh
 
-local_repo=/media/files/github/ctlos/ctlos_repo/
-dest_osdn=creio@storage.osdn.net:/storage/groups/c/ct/ctlos/ctlos_repo/
-dest_keybase=/run/user/1000/keybase/kbfs/public/cvc/ctlos_repo/
+local_repo=/media/files/github/glavos/glavos_repo/
+dest_osdn=creio@storage.osdn.net:/storage/groups/c/ct/glavos/glavos_repo/
+dest_keybase=/run/user/1000/keybase/kbfs/public/cvc/glavos_repo/
 
 if [ "$1" = "-add" ]; then
-  # repo-add -s -v -n -R ctlos_repo.db.tar.gz *.pkg.tar.xz
-  # repo-add -n -R ctlos_repo.db.tar.gz *.pkg.tar.{xz,zst}
-  repo-add -n -R ctlos_repo.db.tar.gz *.pkg.tar.zst
-  rm ctlos_repo.{db,files}
-  cp -f ctlos_repo.db.tar.gz ctlos_repo.db
-  cp -f ctlos_repo.files.tar.gz ctlos_repo.files
+  # repo-add -s -v -n -R glavos_repo.db.tar.gz *.pkg.tar.xz
+  # repo-add -n -R glavos_repo.db.tar.gz *.pkg.tar.{xz,zst}
+  repo-add -n -R glavos_repo.db.tar.gz *.pkg.tar.zst
+  rm glavos_repo.{db,files}
+  cp -f glavos_repo.db.tar.gz glavos_repo.db
+  cp -f glavos_repo.files.tar.gz glavos_repo.files
   apindex .
   ##optional-remove for old repo.db##
   # rm *gz.old{,.sig}
 echo "Repo Up"
 elif [ "$1" = "-clean" ]; then
-  rm ctlos_repo*
+  rm glavos_repo*
   echo "Repo clean"
 elif [ "$1" = "-o" ]; then
   rsync -cauvCLP --delete-excluded --delete "$local_repo"x86_64 "$dest_osdn"
@@ -47,18 +47,18 @@ elif [ "$1" = "-k" ]; then
   rsync -cauvCLP --delete-excluded --delete --exclude={"build",".git*",".*ignore"} "$local_repo"x86_64/ "$dest_keybase"
   echo "rsync keybase repo"
 elif [ "$1" = "-all" ]; then
-  repo-add -n -R ctlos_repo.db.tar.gz *.pkg.tar.zst
-  rm ctlos_repo.{db,files}
-  cp -f ctlos_repo.db.tar.gz ctlos_repo.db
-  cp -f ctlos_repo.files.tar.gz ctlos_repo.files
+  repo-add -n -R glavos_repo.db.tar.gz *.pkg.tar.zst
+  rm glavos_repo.{db,files}
+  cp -f glavos_repo.db.tar.gz glavos_repo.db
+  cp -f glavos_repo.files.tar.gz glavos_repo.files
   apindex .
   rsync -cauvCLP --delete-excluded --delete "$local_repo"x86_64 "$dest_osdn"
   rsync -cauvCLP --delete-excluded --delete --exclude={"build",".git*",".*ignore"} "$local_repo"x86_64/ "$dest_keybase"
   echo "add pkg, rsync all repo"
 else
-  repo-add -n -R ctlos_repo.db.tar.gz *.pkg.tar.zst
-  rm ctlos_repo.{db,files}
-  cp -f ctlos_repo.db.tar.gz ctlos_repo.db
-  cp -f ctlos_repo.files.tar.gz ctlos_repo.files
+  repo-add -n -R glavos_repo.db.tar.gz *.pkg.tar.zst
+  rm glavos_repo.{db,files}
+  cp -f glavos_repo.db.tar.gz glavos_repo.db
+  cp -f glavos_repo.files.tar.gz glavos_repo.files
   echo "Done repo-add pkg"
 fi
